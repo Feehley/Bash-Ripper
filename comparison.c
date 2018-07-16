7327654e78ac054b0a6ce66ff26257cc5ecf6def
John Feehley <johnfeehley@yahoo.com>
2018-07-16 14:27:23 -0400

    Delete comparison.c

--- comparison.c
+++
@@ -1,7 +0,0 @@
-//John Feehley
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-int main (int argc, char* argv[]) {if (argc < 3) {return 1;}int x; x = strcmp(argv[1], argv[2]);if (x > 0){return 1;}else if(x < 0){return 1;} else{return 0;}}
